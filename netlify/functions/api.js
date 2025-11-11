ca998b463a01360dea9597286872f5a6a9ba10a3
RKNSYAH <manusiakocak430@gmail.com>
2025-11-12 06:09:41 +0700

    netlify fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,44 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 import router from "../../router/router";
+import mongoose from "mongoose";
+
 const api = express();
 
-api.use("/api/", router);
+api.use(cors());
+api.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+let conn = null;
+
+const connectToDatabase = async () => {
+  if (conn == null) {
+    console.log("Creating new connection to MongoDB...");
+    try {
+      conn = await mongoose.connect(uri, {
+        // These options are good for serverless
+        serverSelectionTimeoutMS: 5000 
+      });
+      console.log("Successfully connected to MongoDB!");
+      return conn;
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    }
+  }
+
+  console.log("Using cached MongoDB connection.");
+  return conn;
+};
+
+
+app.use('/api', async (req, res, next) => {
+  try {
+    await connectToDatabase();
+    next(); 
+  } catch (error) {
+    res.status(500).send({ msg: "Database connection failed" });
+  }
+}, router);
 
 export const handler = serverless(api);
