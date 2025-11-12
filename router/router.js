bd3ba2c9b859f6ce5255b207c23a645b6534059c
RKNSYAH <manusiakocak430@gmail.com>
2025-11-13 04:50:43 +0700

    add logout

--- router/router.js
+++ router/router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getWorkers, loginSupervisor, loginWorker, addDummyDetection, addDetection } from "../controllers/controller.js";
+import { getWorkers, loginSupervisor, loginWorker, addDummyDetection, addDetection, logoutWorker } from "../controllers/controller.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = Router()
@@ -7,6 +7,7 @@ const router = Router()
 router.get('/worker/:id', verifyToken, getWorkers)
 router.post('/supervisor/login', loginSupervisor)
 router.post('/worker/login', loginWorker)
+router.post('/worker/logout', logoutWorker)
 router.get('/detection', addDummyDetection)
 router.post('/detection', verifyToken, addDetection )
 // router.post('/user', login)
